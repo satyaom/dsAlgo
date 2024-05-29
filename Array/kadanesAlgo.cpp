43e7c117231f49da2dadabf3b8836b21c0443663
satyam pandey <pandey.satyam@graphy.com>
2024-05-29 10:51:34 +0530

    done with kadanes algo

---
+++ Array/kadanesAlgo.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+using namespace std;
+
+int maxSubArraySum(int ar[], int n) {
+  int maxTillNow = 0, maxNow = 0;
+  for(int i = 0; i < n; i++) {
+    maxNow = maxNow+ar[i];
+    if(maxTillNow < maxNow) maxTillNow = maxNow;
+    if(maxNow < 0) maxNow = 0;
+  }
+  return maxTillNow;
+}
+
+int main() {
+  int arr[] = {3, 2, -3, 2, 3};
+  printf("%d", maxSubArraySum(arr, 5));
+  return 0;
+}
